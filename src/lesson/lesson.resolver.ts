79682f4867ab1c8222f694a646b5930b16a87d81
Abhishek Tiwari <abhie.lp@gmail.com>
2021-11-06 23:54:06 +0530

    Add GraphQL mutation to create lesson

--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -1,8 +1,12 @@
-import { Query, Resolver } from "@nestjs/graphql";
+import { Args, Mutation, Query, Resolver } from "@nestjs/graphql";
+import { LessonService } from "./lesson.service";
 import { LessonType } from "./lesson.type";
 
 @Resolver(_of => LessonType)
 export class LessonResolver {
+    constructor(
+        private lessonService: LessonService
+    ) {}
     @Query(_returns => LessonType)
     lesson() {
         return {
@@ -11,5 +15,14 @@ export class LessonResolver {
             startDate: "1996-09-01",
             endDate: "2021-09-01"
         }
+    }
+
+    @Mutation(_returns => LessonType)
+    createLesson(
+        @Args("name") name: string,
+        @Args("startDate") startDate: string,
+        @Args("endDate") endDate: string
+    ) {
+        return this.lessonService.createLesson(name, startDate, endDate)
     }
 }
