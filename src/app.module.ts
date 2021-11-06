8e8ea46120259fc9c6b40fa46abc303e106e3520
Abhishek Tiwari <abhie.lp@gmail.com>
2021-11-06 23:25:21 +0530

    Connect to MongoDB and add Lesson entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,32 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { GraphQLModule } from "@nestjs/graphql";
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Lesson } from './lesson/lesson.entity';
 import { LessonModule } from './lesson/lesson.module';
 
 @Module({
-  imports: [GraphQLModule.forRoot({
-    autoSchemaFile: true,
-  }), LessonModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.env.conf.${process.env.STAGE}`
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: "mongodb",
+        url: configService.get("DB_URL"),
+        synchronize: true,
+        useUnifiedTopology: true,
+        entities: [
+          Lesson,
+        ]
+      })
+    }),
+    GraphQLModule.forRoot({
+      autoSchemaFile: true,
+    }),
+    LessonModule
+],
 })
 export class AppModule {}
