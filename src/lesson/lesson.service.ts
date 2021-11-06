d967e4304f961d5cadf01ab8cc3b3efb05ebc050
Abhishek Tiwari <abhie.lp@gmail.com>
2021-11-07 00:09:48 +0530

    Validation for mutation parameters

--- src/lesson/lesson.service.ts
+++ src/lesson/lesson.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Lesson } from './lesson.entity';
 import { v4 as uuid } from "uuid";
+import { CreateLessonInput } from './lesson.input';
 
 @Injectable()
 export class LessonService {
@@ -16,8 +17,9 @@ export class LessonService {
     }
 
     async createLesson(
-        name: string, startDate: string, endDate: string
+        createLessonInput: CreateLessonInput
     ): Promise<Lesson> {
+        const { name, startDate, endDate } = createLessonInput
         const lesson = this.lessonRepository.create({
             id: uuid(),
             name,
